d7262549b51d61156b0ac2422327c75baf689e22
Ananmayd <ananmay1603@gmail.com>
2024-06-17 01:36:25 +0530

    uff

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,10 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));   //handle data comes using url
 app.use(bodyParser.json());
 app.use(cors({
-    origin:'https://inventory-13sz.vercel.app', 
+    origin:'https://inventory-13sz.vercel.app/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }));
-// , "https://AnInvent.vercel.app"
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //Routes Middleware
